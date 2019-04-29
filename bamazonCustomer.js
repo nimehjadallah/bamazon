e3a2bae698e7c4563a317aa06dd01c50c8d0e313
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-04-29 16:09:09 -0400

    getting all products info form the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,5 +17,22 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    // createProduct();
+    // start();
   });
+
+  function start(){
+    //prints the items for sale and their details
+    connection.query('SELECT * FROM products', function(err, res){
+      if(err) throw err;
+    
+      console.log('_.~"~._.~"~._.~Welcome to Bamazon~._.~"~._.~"~._')
+      console.log('----------------------------------------------------------------------------------------------------')
+    
+      for(var i = 0; i<res.length;i++){
+        console.log("ID: " + res[i].item_id + " || " + "Product: " + res[i].product_name + " || " + "Department: " + res[i].department_name + " || " + "Price: " + res[i].price + " || " + "Quantity: " + res[i].stock_quantity);
+        console.log('--------------------------------------------------------------------------------------------------')
+      }
+    })
+    }
+
+    start();
